66070fb3bb21ff69b4d2b1ddf25d3ab89d9770d6
Hasnain Ali Baloch <hasnainalibaloch@outlook.com>
2022-12-18 01:54:43 +0500

    top nav update

--- Screens/SearchAbode/TopNav/AutoComplete.tsx
+++ Screens/SearchAbode/TopNav/AutoComplete.tsx
@@ -1,26 +1,48 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useCallback, useRef, useState} from 'react';
 import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
 import {GooglePlacesAutocompleteRef} from 'react-native-google-places-autocomplete';
+import {theme} from '../../../theme';
 
 const AutoCompleteInput = () => {
-  const ref = useRef<any>();
+  const [location, setLocation] = useState<Location>({lat: '', lng: ''});
+  const ref = useRef<GooglePlacesAutocompleteRef>(null);
 
-  // useEffect(() => {
-  //   ref.current?.setAddressText('');
-  // }, []);
+  interface Location {
+    lat: string;
+    lng: string;
+  }
+
+  const handleAutoComplete = useCallback((loc: Location) => {
+    setLocation(loc);
+  }, []);
 
   return (
     <GooglePlacesAutocomplete
+      styles={{
+        textInput: {
+          color: `${theme?.colors?.secondary}`,
+          fontSize: 16,
+          borderRadius: 10,
+          paddingLeft: 40,
+          paddingRight: 10,
+          borderColor: `${theme?.colors?.secondary}`,
+          borderWidth: 0.3,
+          marginTop: 2,
+        },
+      }}
       ref={ref}
       placeholder="Search"
-      onPress={(data, details = null) => {
-        // 'details' is provided when fetchDetails = true
-        console.log(data, details);
+      GooglePlacesDetailsQuery={{fields: 'geometry'}}
+      onPress={(data: any, details: any = null) => {
+        handleAutoComplete(details?.geometry?.location);
       }}
+      fetchDetails={true}
       query={{
         key: 'AIzaSyD2SZ1i_nM8v6Qqlbb6TKjO6sY_Di4LpqI',
         language: 'en',
       }}
+      enablePoweredByContainer={false}
+      minLength={2}
     />
   );
 };
--- Screens/SearchAbode/TopNav/index.tsx
+++ Screens/SearchAbode/TopNav/index.tsx
@@ -15,12 +15,12 @@ const TopNav = () => {
         style={styles(theme).buttonBg}>
         {false ? (
           <RowVertical
-            size="16"
-            color={theme.colors.background}
+            size="20"
+            color={theme.colors.primary}
             variant="Bulk"
           />
         ) : (
-          <Map1 size="20" color={theme.colors.background} variant="Bulk" />
+          <Map1 size="24" color={theme.colors.primary} variant="Bold" />
         )}
       </TouchableOpacity>
       <View style={styles().placesContainer}>
@@ -30,7 +30,7 @@ const TopNav = () => {
         touchSoundDisabled={true}
         activeOpacity={0.8}
         style={styles(theme).buttonBg}>
-        <Profile size="20" color={theme.colors.background} variant="Bold" />
+        <Profile size="24" color={theme.colors.primary} variant="Bold" />
       </TouchableOpacity>
     </View>
   );
@@ -45,23 +45,33 @@ const styles = (props?: any) =>
       flex: 1,
       position: 'absolute',
       top: 0,
-      padding: 10,
+      padding: 5,
     },
 
     buttonBg: {
-      backgroundColor: `${props?.colors?.primary}`,
+      backgroundColor: `${props?.colors?.background}`,
       height: 40,
       width: 40,
       borderRadius: 100,
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
-      marginTop: 3
+      marginTop: 3,
+      shadowColor: `${props?.colors?.primary}`,
+      shadowOffset: {
+        width: 10,
+        height: 13,
+      },
+      shadowOpacity:  0.17,
+      shadowRadius: 3.05,
+      elevation: 6,
+      borderColor: `${props?.colors?.secondary}`,
+      borderWidth: 0.3,
     },
 
     placesContainer: {
-      flex:1,
-      marginHorizontal: 15,
+      flex: 1,
+      marginHorizontal: 5,
     },
   });
 
